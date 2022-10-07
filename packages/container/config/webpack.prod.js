d4d1c1c46c59fbcae1c06f8b22a659beff2e2e34
hazigabriel <hazigabriel@gmail.com>
2022-10-07 12:10:05 +0300

    fixed container prod config typo

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,7 +3,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
 
-const domanin = process.env.PROUCTION_DOMAIN;
+const domanin = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode: 'production',
