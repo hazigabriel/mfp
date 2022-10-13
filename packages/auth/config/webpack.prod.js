74d276d788a1845e5ad8858070703faf4c85c9c7
hazigabriel <hazigabriel@gmail.com>
2022-10-13 15:54:30 +0300

    add auth sub-app

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -1,19 +1,13 @@
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
-const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
+const commonConfig = require('./webpack.common')
 
-const devConfig = {
-  mode: 'development',
+const prodConfig = {
+  mode: 'production',
   output: {
-    publicPath: 'http://localhost:8081/'
-  },
-  devServer: {
-    port: 8081,
-    historyApiFallback: {
-      index: '/index.html'
-    }
+    filename: '[name].[contenthash].js',
+    publicPath: '/auth/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
@@ -23,13 +17,8 @@ const devConfig = {
         './MarketingApp': './src/bootstrap'
       },
       shared: packageJson.dependencies
-
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    }),
-
+    })
   ]
 }
 
-module.exports = merge(commonConfig, devConfig)
+module.exports = merge(commonConfig, prodConfig)
